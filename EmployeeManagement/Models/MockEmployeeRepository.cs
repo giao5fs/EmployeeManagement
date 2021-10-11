1556e2075ec6d39966f64358b00bd4a6f83d92a9
giao5fs <giao5fs@GIAO5FS>
2021-10-11 23:35:03 +0700

    Add project files.

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository: IEmployeeRepository
+    {
+
+        private List<Employee> employees;
+        public MockEmployeeRepository()
+        {
+            employees = new List<Employee> { 
+                new Employee(){Id=1, Name="A", Department="HR", Email="a@a.com"},
+                new Employee(){Id=2, Name="B", Department="IT", Email="b@b.com"},
+                new Employee(){Id=3, Name="C", Department="PQ", Email="c@a.com"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return employees.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,50 @@
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private readonly IConfiguration config;
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(x => x.EnableEndpointRouting = false).AddXmlSerializerFormatters();
+
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            //app.UseMvcWithDefaultRoute();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            //});
+        }
+    }
+}
